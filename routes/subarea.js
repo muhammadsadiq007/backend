355fc95d4be352bbfdf64a92e5ae83b27ad89d30
muhammadsadiq007 <ssadiq500@gmail.com>
2025-02-07 21:55:22 +0500

    update

--- routes/subarea.js
+++ routes/subarea.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import { getSubarea, addSubarea, getSubareas, editSubarea } from "../controllers/subareaController.js";
+import { getSubarea, addSubarea, getSubareas, editSubarea } from "../controllers/subareacontroller.js";
 
 const   router = express.Router();
 
